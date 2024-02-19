885cb003f29e4406cbf2b73e347884549831f4c7
GianMarchetti <gian11marchetti@gmail.com>
2024-02-19 16:39:28 -0300

    Optimized code

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 import Logo from '../assets/imgs/logoCasaBonita.jpg'
 import { ShoppingBagIcon, PencilSquareIcon } from '@heroicons/react/20/solid'
 import { Big_Shoulders_Display } from 'next/font/google'
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useAnimate, stagger, motion } from "framer-motion";
 import { Button } from '@nextui-org/react'
 import { useShoppingCart } from "use-shopping-cart"
@@ -75,127 +75,71 @@ const linksDesk = [
 }
 ]
 
-function useMenuAnimationMob(isOpen : boolean) {
-  const [scopeMob, animateMob] = useAnimate();
-
-  useEffect(() => {
-    animateMob(".arrow", { rotate: isOpen ? 180 : 0 }, { duration: 0.2 });
-
-    animateMob(
-      "ul",
-      {
-        clipPath: isOpen
-          ? "inset(0% 0% 0% 0% round 10px)"
-          : "inset(10% 50% 90% 50% round 10px)"
-      },
-      {
-        type: "spring",
-        bounce: 0,
-        duration: 0.5
-      }
-    );
-
-    animateMob(
-      "li",
-      isOpen
-        ? { opacity: 1, scale: 1, filter: "blur(0px)" }
-        : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
-      {
-        duration: 0.2,
-        delay: isOpen ? staggerMenuItems : 0
-      }
-    );
-  }, [isOpen]);
-
-  return scopeMob;
-}
-
 const linksMob = [
-{
-  label: 'Inicio',
-  route: '/',
-},
-{
-  label: 'Blog',
-  route: `https://www.instagram.com/yas.casabonita/`
-},
-{
-  label: 'Productos',
-  route: '/components/products'
-},
-{
-  label: 'Macramé',
-  route: '/components/products'
-},
-{
-  label: 'Asesorias',
-  route: '/components/asesorias'
-},
-{
-  label: 'Talleres',
-  route: '/components/talleres'
-},
-{
-  label: 'Contacto',
-  route: '/components/contact'
-},
-{
-  label: 'Guias Gratis',
-  route: '/components/guias'
-}
-]
-
+  {
+    label: 'Inicio',
+    route: '/',
+  },
+  {
+    label: 'Macramé',
+    route: '/components/products'
+  },
+  {
+    label: 'Asesorias',
+    route: '/components/asesorias'
+  },
+  {
+    label: 'Talleres',
+    route: '/components/talleres'
+  },
+  {
+    label: 'Contacto',
+    route: '/components/contact'
+  },
+  {
+    label: 'Guias Gratis',
+    route: '/components/guias'
+  },
+  {
+    label: 'Blog',
+    route: `https://www.instagram.com/yas.casabonita/`
+  }
+  ]
 
 export function Navigation () {
   const { cartCount } = useShoppingCart()
   const [isOpen, setIsOpen] = useState(false);
   const scope = useMenuAnimation(isOpen);  
-  const scopeMob = useMenuAnimationMob(isOpen);  
   const pathname = usePathname()
+  const ref = useRef()
+
+  const closeOpenMenus = (e) => {
+    if (isOpen && !scope.current?.contains(e.target)){
+      setIsOpen(false)
+    }
+  }
+
+  document.addEventListener('mousedown', closeOpenMenus)
 
   if (pathname.startsWith('/studio')) return null
 
   return (
     <header className={styles.header}>
 
-{/* -----------------Menu Desktop------------------------------- */}
-
       <nav className={styles.nav}>
 
-        <div className={styles.dropMob}>
-            <div className="menu" ref={scopeMob} >
-            <motion.button 
-                whileTap={{ scale: 0.97 }}
-                onClick={() => setIsOpen(!isOpen)}
-            >
-                <div className="arrow" style={{ transformOrigin: "50% 55%" }}>
-                    <svg width="15" height="15" viewBox="0 0 20 20">
-                        <path d="M0 7 L 20 7 L 10 16" />
-                    </svg>
-                </div>
-            </motion.button> 
-                  <ul style={{
-                      pointerEvents: isOpen ? "auto" : "none",
-                      clipPath: "inset(10% 50% 90% 50% round 10px)"
-                    }} className="dropMob">
-                      {linksMob.map(({ label, route }) => (
-                      <li key={route}>
-                        <Link href={route}>
-                          {label}
-                        </Link>
-                      </li>
-                    ))}
-                    </ul>{" "}
-            </div>
-          </div>
+{/* ---------------- Logo Desktop --------------------------- */}
 
         <Link href='/'>
           <Image priority alt='Logo de Casa Bonita' src={Logo} className={styles.logoImg} width={200} height={200} />
         </Link>
 
-        <div className={styles.links}>             
-          <div className={styles.dropDesk}>
-            <Link href={`/`} >Inicio</Link>
+        <div className={styles.links}>
+
+            <Link href={`/`} className={styles.dropDesk}>Inicio</Link>
+
+{/* ---------------- Dropdown Menu --------------------------- */}
+
             <div className="menu" ref={scope} >
             <motion.button 
                 whileTap={{ scale: 0.97 }}
@@ -207,10 +151,14 @@ export function Navigation () {
                     </svg>
                 </div>
             </motion.button>
+
+{/* ---------------- Links Dropdown Desktop --------------------------- */}
+            
             <ul style={{
               pointerEvents: isOpen ? "auto" : "none",
               clipPath: "inset(10% 50% 90% 50% round 10px)"
-              }}>
+              }}
+              className={styles.dropDesk}>
                 {linksDesk.map(({ label, route }) => (
                 <li key={route}>
                   <Link href={route}>
@@ -219,17 +167,45 @@ export function Navigation () {
                 </li>
               ))}
             </ul>{" "}
+
+{/* ---------------- Links Dropdown Mobile --------------------------- */}
+
+            <ul style={{
+              pointerEvents: isOpen ? "auto" : "none",
+              clipPath: "inset(10% 50% 90% 50% round 10px)"
+              }}
+              className={styles.dropMob}>
+                {linksMob.map(({ label, route }) => (
+                <li key={route}>
+                  <Link href={route}>
+                    {label}
+                  </Link>
+                </li>
+              ))}
+            </ul>{" "}
             </div>
-            <Link href={`/components/guias`}> <span>Guias</span><span className={styles.Free}><p className={Big.className}>GRATIS</p></span></Link>
-            <Link href={`https://www.instagram.com/yas.casabonita/`}>Blog</Link>
-            <Link href={`/components/contact`}>Contacto</Link>
-            </div>
+
+{/* ---------------- Links Desktop --------------------------- */}
+
+            <Link href={`/components/guias`} className={styles.dropDesk}> <span>Guias</span><span className={styles.Free}><p className={Big.className}>GRATIS</p></span></Link>
+            <Link href={`https://www.instagram.com/yas.casabonita/`} className={styles.dropDesk}>Blog</Link>
+            <Link href={`/components/contact`} className={styles.dropDesk}>Contacto</Link>
+
+{/* ---------------- Logo Mobile --------------------------- */}
+        
+            <Link href='/' className={styles.logoImgMob}>
+              <Image alt='Logo de Casa Bonita' src={Logo} className={styles.logoImgMob} width={200} height={200} />
+            </Link>
+
+{/* ---------------- Carrito --------------------------- */}
+
             <Link href={`/components/cart`} className='h-fit w-fit'>
-              <Button size='sm' className='bg-amber-100 lg:bg-transparent hover:bg-amber-100 sm:bg-amber-100 h-fit w-fit ml-1'>
+              <Button size='sm' className='bg-amber-100 lg:bg-transparent hover:bg-amber-100 sm:bg-amber-100 h-fit w-fit'>
                 <span className="ml-1 text-sm font-bold text-black ">{cartCount}</span>
                 <ShoppingBagIcon className="h-6 w-6" />
               </Button>
             </Link>
+
         </div>
 
       </nav>
--- src/app/components/contact/page.tsx
+++ src/app/components/contact/page.tsx
@@ -16,7 +16,7 @@ export default function Contact(){
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const endpoint = 'https://formspree.io/f/xpzvqqav';
+        const endpoint = 'https://formspree.io/f/xyyreoya';
 
         try {
         await axios.post(endpoint, {
@@ -36,57 +36,61 @@ export default function Contact(){
     };
 
     return(
-        <main className='flex flex-col-reverse lg:flex-row'>
+        <main className='flex flex-col'>
 
-            <div className={styles.contact}>
+            <h1>Contactanos</h1>
 
-                <div className={styles.fondo}>     
-                    
-                    <h1>Contactanos</h1>
-                         
-                    <h2 className={styles.infoTitle}>Te ayudaremos con lo que necesites a la brevedad</h2>
-         
-                    <div className={styles.info}>
-                        <div className={styles.infoDiv}>
-                            <p className={styles.infoTitle}>Ubicación</p>
-                            <p className={styles.infoText}>Santa Fe, Arg</p>
+            <section className='flex flex-col-reverse lg:flex-row'>
+
+                <div className={styles.contact}>
+
+                    <div className={styles.fondo}>     
+                        
+                            
+                        <h2 className={styles.infoTitle}>Te ayudaremos con lo que necesites a la brevedad</h2>
+            
+                        <div className={styles.info}>
+                            <div className={styles.infoDiv}>
+                                <p className={styles.infoTitle}>Ubicación</p>
+                                <p className={styles.infoText}>Santa Fe, Arg</p>
+                            </div>
+                            <div className={styles.infoDiv}>
+                                <p className={styles.infoTitle}>Correo electrónico</p>
+                                <p className={styles.infoText}>yasminlmarchetti@gmail.com</p>
+                            </div>
+                            <div className={styles.infoDiv}>
+                                <p className={styles.infoTitle}>Instagram</p>
+                                <p className={styles.infoText}><a href={`https://www.instagram.com/yas.casabonita/`}>@yas.casabonita</a></p>
+                            </div>
                         </div>
-                        <div className={styles.infoDiv}>
-                            <p className={styles.infoTitle}>Correo electrónico</p>
-                            <p className={styles.infoText}>yasminlmarchetti@gmail.com</p>
+
+                        <div className={styles.fondoText}>
+                            <p>Tambien te dejamos un link con destino a nuestro whatsapp, en el que podes comunicarte con nosotros y dejarnos las preguntas que tengas.</p>
+                            <Link href='https://wa.link/duppll'>
+                                <Button className='bg-yellow-200 text-black w-fit shadow shadow-black hover:bg-yellow-500 mt-2' >Ir a WhatsApp</Button>
+                            </Link>
                         </div>
-                        <div className={styles.infoDiv}>
-                            <p className={styles.infoTitle}>Instagram</p>
-                            <p className={styles.infoText}><a href={`https://www.instagram.com/yas.casabonita/`}>@yas.casabonita</a></p>
-                        </div>
+
                     </div>
 
-                    <div className={styles.fondoText}>
-                        <p>Tambien te dejamos un link con destino a nuestro whatsapp, en el que podes comunicarte con nosotros y dejarnos las preguntas que tengas.</p>
-                        <Link href='https://wa.link/duppll'>
-                            <Button className='bg-yellow-200 text-black w-fit shadow shadow-black hover:bg-yellow-500 mt-2' >Ir a WhatsApp</Button>
-                        </Link>
-                    </div>
+                
+                </div>
+            
+                <div className="w-full flex flex-col items-center p-8 lg:w-1/2" id="contact">
+                
+                    <form onSubmit={handleSubmit} className="form w-full justify-between items-center p-8">
+                        <input type="hidden" name="contact_number"/>
+                        <div className="flex justify-between items-center md:flex-col gap-2 w-full mb-2">
+                            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Nombre y Apellido" className="bg-white h-10 mb-2 text-black rounded-md p-4 w-full sm:p-1 " />
+                            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" className="bg-white h-10 mb-2 text-black rounded-md p-4 w-full sm:p-1 " />
+                        </div>
+                        <textarea aria-label="text" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Mensaje" className="bg-white h-32 text-black rounded-md p-4 sm:p-1 w-full"></textarea>
+                        <Button className="text-black bg-amber-100 border-0 hover:bg-amber-200 rounded-2xl mt-4 p-2 sm:p-1">
+                            <input type="submit" value="Send" className="cursor-pointer w-full" />
+                        </Button>
+                    </form>
 
                 </div>
-
-            
-            </div>
-            
-            <section className="w-full flex flex-col items-center p-8 lg:w-1/2" id="contact">
-            
-                <h2 className="text-3xl font-bold sm:text-4xl">Pongamosnos en Contacto</h2>
-                <form onSubmit={handleSubmit} className="form w-full justify-between items-center p-8">
-                    <input type="hidden" name="contact_number"/>
-                    <div className="flex justify-between items-center md:flex-col gap-2 w-full mb-2">
-                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Nombre y Apellido" className="bg-white h-10 mb-2 text-black rounded-md p-4 w-full sm:p-1 " />
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" className="bg-white h-10 mb-2 text-black rounded-md p-4 w-full sm:p-1 " />
-                    </div>
-                    <textarea aria-label="text" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Mensaje" className="bg-white h-32 text-black rounded-md p-4 sm:p-1 w-full"></textarea>
-                    <Button className="text-black bg-amber-100 border-0 hover:bg-amber-200 rounded-2xl mt-4 p-2 sm:p-1">
-                        <input type="submit" value="Send" className="cursor-pointer w-full" />
-                    </Button>
-                </form>
 
             </section>
 
